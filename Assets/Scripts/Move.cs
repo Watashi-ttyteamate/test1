ec674e436131fe8394416bf6d12d16fca9b2c697
Watashi-ttyteamate <35679063+Watashi-ttyteamate@users.noreply.github.com>
2018-01-22 18:23:49 -0700

    Initial

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Move : MonoBehaviour {
+
+
+	private float thrust;
+	private float boost;
+	private float pitch;
+	private float yaw;
+	private float roll;
+	private int capacity;
+	public int startCapacity = 1000;
+	[SerializeField]float YawSensitivity = 1;
+	[SerializeField]float RollSensitivity = 1;
+	[SerializeField]float PitchSensitivity = 1;
+	[SerializeField]float StrafeSensitivity = 1;
+	[SerializeField]float ThrustMod;
+	[SerializeField]float boostStrength;
+
+	public Rigidbody rb;
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody>();
+		capacity = startCapacity;
+	}
+
+	void FixedUpdate ()
+	{
+		
+		pitch = Input.GetAxis("Vertical");
+		roll = Input.GetAxis("Horizontal");
+		yaw = Input.GetAxis ("Strafe");
+		rb.AddRelativeTorque(pitch * PitchSensitivity, 0, -roll * RollSensitivity);
+		rb.AddRelativeForce (yaw * YawSensitivity, 0, 0);
+
+		boost = Input.GetAxis ("Boost");
+
+		if (capacity >= 1000)
+		{
+			if (boost == 1) 
+			{
+				rb.AddForce(transform.forward * boostStrength);
+				capacity = capacity - 1000;
+			}
+		}
+
+		thrust = Input.GetAxis ("Thrust");
+		rb.AddForce(transform.forward * thrust * ThrustMod);
+
+		if (capacity <= 1000) 
+		{
+			Debug.Log (capacity);
+			capacity = capacity + 2;
+		}
+	}
+}
---
+++ Assets/Scripts/ShootingMechanic.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingMechanic : MonoBehaviour {
+
+	public Transform Player;
+	public Transform bulletPosition;
+	public GameObject Bullet;
+	private float bulletSpeed = 3000f;
+	private float fire;
+	private float fireBuffer = 0;
+	public float fireRate = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		fire = Input.GetAxis ("Fire1");
+
+		if (fireBuffer == 0)
+		{
+
+			if (fire == 1) 
+			{
+				fireBuffer = 1;
+
+				GameObject bulletclone = (GameObject)Instantiate (Bullet, bulletPosition.position, bulletPosition.rotation); //Spawns Bullet Clone
+
+				Rigidbody bulletRigid = bulletclone.GetComponent<Rigidbody> ();
+
+				bulletRigid.AddRelativeForce (bulletPosition.transform.forward * bulletSpeed);
+
+				Destroy (bulletclone, 5f); //Destroys Warning Clone
+
+				Invoke ("FireBuffer",fireRate);
+			}
+		}
+	}
+
+	void FireBuffer ()
+	{
+		fireBuffer = 0;
+	}
+}
